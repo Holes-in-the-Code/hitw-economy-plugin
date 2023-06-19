cd876056d36a35a93072f3fc7abb0ad1bd7801df
FubyCutie <roset280905@gmail.com>
2023-06-19 17:35:31 +0100

    re-did gameType filte

--- src/main/kotlin/com/hitc/hitweconomyplugin/main/utils/CreditsUtils.kt
+++ src/main/kotlin/com/hitc/hitweconomyplugin/main/utils/CreditsUtils.kt
@@ -8,18 +8,14 @@ object CreditsUtils {
 
     fun addCredits(gameType: GameType, score: Int, ePlayer: EPlayer) : Int {
         val scores = GameFileUtils.loadScores(ePlayer)
-        val filteredScores = MutableList<Score?>(0) { null }
-        for (currentScore in scores) {
-            if (currentScore?.gameType == gameType) {
-                filteredScores.add(currentScore)
-            }
-        }
+        val filteredScores = scores.filter { it?.gameType == gameType }
+
         val credits = creditsAlgorithm1(filteredScores, score)
         GameFileUtils.addCredits(ePlayer, credits)
         return credits
     }
 
-    private fun creditsAlgorithm1(filteredScores : MutableList<Score?>, score : Int) : Int {
+    private fun creditsAlgorithm1(filteredScores : List<Score?>, score : Int) : Int {
         val amount = filteredScores.size
         return if (amount <= 10) score else score/10
     }
