b9c2b73b843c4c17e9f051aa3c575df7fe5d11e5
FubyCutie <roset280905@gmail.com>
2023-06-19 04:21:36 +0100

    added data version information

--- src/main/kotlin/com/hitc/hitweconomyplugin/main/utils/GameFileUtils.kt
+++ src/main/kotlin/com/hitc/hitweconomyplugin/main/utils/GameFileUtils.kt
@@ -1,23 +1,27 @@
 package com.hitc.hitweconomyplugin.main.utils
 
+import com.hitc.hitweconomyplugin.main.core.DataFile
 import com.hitc.hitweconomyplugin.main.core.EPlayer
 import com.hitc.hitweconomyplugin.main.core.PlayerData
 import com.hitc.hitweconomyplugin.main.core.Score
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.encodeToJsonElement
 import org.bukkit.entity.Player
 import java.io.File
-import java.lang.Exception
 import java.nio.file.Files
 import java.util.*
+import java.util.zip.DataFormatException
+import kotlin.Exception
 
 object GameFileUtils {
 
     fun loadScores(ePlayer: EPlayer,
                    calendar: Calendar = Calendar.getInstance(TimeZone.getTimeZone("EST"))
     ) : MutableList<Score?> {
-        var scores : MutableList<Score?> = MutableList(0) { null }
+        var scores = MutableList<Score?>(0) { null }
 
         var f = ePlayer.dailyDataFile
         if (calendar.get(Calendar.DAY_OF_YEAR) != f.second.get(Calendar.DAY_OF_YEAR)) {
@@ -26,10 +30,15 @@ object GameFileUtils {
         }
 
         val text = f.first.readText()
-        if (text.isNotBlank()) try {
-            scores = Json.decodeFromString<MutableList<Score?>>(text)
-        } catch (e: Exception) {
-            e.printStackTrace()
+        if (text.isNotBlank()) {
+            val dataJson = Json.decodeFromString<DataFile>(text)
+            // TODO make a system that maps dataVersions to dataFixers
+            if (dataJson.dataVersion == 0) {
+                scores = Json.decodeFromJsonElement<MutableList<Score?>>(dataJson.data)
+            }
+            else {
+                throw Exception("Invalid data version")
+            }
         }
         return scores
     }
@@ -38,22 +47,36 @@ object GameFileUtils {
         val f = ePlayer.dataFile
         val text = f.readText()
         var playerData = PlayerData(0,0)
-        if (text.isNotBlank()) try {
-            playerData = Json.decodeFromString<PlayerData>(text)
-        } catch (e: Exception) {
-            e.printStackTrace()
+        if (text.isNotBlank()) {
+            val dataJson = Json.decodeFromString<DataFile>(text)
+            // TODO make a system that maps dataVersions to dataFixers
+            if (dataJson.dataVersion == 0) {
+                playerData = Json.decodeFromJsonElement<PlayerData>(dataJson.data)
+            }
+            else {
+                throw Exception("Invalid data version")
+            }
         }
         return playerData
     }
 
     fun savePlayerData(ePlayer: EPlayer, playerData: PlayerData) {
         val f = ePlayer.dataFile
-        val playerDataText = Json.encodeToString(playerData)
-        f.writeText(playerDataText)
+        val playerDataElement = Json.encodeToJsonElement(playerData)
+        val dataJson = DataFile(0, playerDataElement)
+
+        f.writeText(Json.encodeToString(dataJson))
     }
 
     fun addCredits(ePlayer: EPlayer, credits : Int) {
-        val playerData = loadPlayerData(ePlayer)
+        val playerData : PlayerData
+        try {
+             playerData = loadPlayerData(ePlayer)
+        } catch (e : Exception) {
+            e.printStackTrace()
+            ePlayer.player.player.sendMessage("§cThere was an error in saving this game, report this to staff.")
+            return
+        }
         playerData.credits += credits
         playerData.creditsEarned += credits
         savePlayerData(ePlayer, playerData)
@@ -65,13 +88,21 @@ object GameFileUtils {
             f = getDailyDataFile(ePlayer.player.player)
             ePlayer.dailyDataFile = f
         }
-        val scoresText = Json.encodeToString(scores)
-        f.first.writeText(scoresText)
+        val scoresElement = Json.encodeToJsonElement(scores)
+        val dataJson = DataFile(0, scoresElement)
+        f.first.writeText(Json.encodeToString(dataJson))
     }
 
     fun appendScore(ePlayer: EPlayer, score : Score) {
         val calendar = Calendar.getInstance(TimeZone.getTimeZone("EST"))
-        val currentScores = loadScores(ePlayer, calendar)
+        val currentScores : MutableList<Score?>
+        try {
+             currentScores = loadScores(ePlayer, calendar)
+        } catch (e : Exception) {
+            e.printStackTrace()
+            ePlayer.player.player.sendMessage("§cThere was an error in saving this game, report this to staff.")
+            return
+        }
         currentScores.add(score)
         saveScores(ePlayer, currentScores, calendar)
     }
